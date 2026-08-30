ffd65370135fd001545860c5a378f6b2c4454485
SodiumChl <michaelgonzalez684@gmail.com>
2026-08-29 21:13:12 -0500

    Added Gravity to the player// WIP Jump
    Added manual gravity to player since no rigidbody and WIP jump code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,31 +4,37 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public InputActionReference movePlayer; // Player Move input
+    public InputActionReference playerJump; // Player Jump input
     public InputActionReference playerCamera;// Camera Look input
-    public CharacterController playerControl; //Replacement for rigidbody on moving player
+    public CharacterController playerControl; //Replacement for rigidbody on moving player, solves problem of movement issues with objects
+        
     public float playerSpeed = 5f;
+    public float playerGravity = -9f;
+    float fallSpeed;
     Transform cameraTransform;
+   
 
     private void OnEnable()
     {
         movePlayer.action.Enable();
         playerCamera.action.Enable();
+        playerJump.action.Enable();
     }
     private void OnDisable()
     {
         movePlayer.action.Disable();
         playerCamera.action.Disable();
+        playerJump.action.Disable();
     }
     void Start()
     {
-        //solves problem of cameratransform dissapearing when you delete the object and replace with prefab
-        //Basically makes cameraTransform equal to the tranform of the object with "MainCamera" Tag 
-        cameraTransform = Camera.main.transform;
+      cameraTransform = Camera.main.transform;//solves problem of cameratransform dissapearing when you delete the object and replace with prefab
+                                                      //Basically makes cameraTransform equal to the tranform of the object with "MainCamera" Tag
     }
 
     public void Update()
-    {   //saves the info of how the player is moving into moveInput
-        Vector2 moveInput = movePlayer.action.ReadValue<Vector2>();
+    {   
+        Vector2 moveInput = movePlayer.action.ReadValue<Vector2>();//saves the info of how the player is moving into moveInput
         float x = moveInput.x;
         float z = moveInput.y; //so that player doesnt go flying when you press W
 
@@ -36,47 +42,30 @@ public class PlayerMovement : MonoBehaviour
         Vector3 cameraForward = cameraTransform.forward; //cameraForward now stores the coords for where the camera is facing
         Vector3 cameraRight = cameraTransform.right; //cameraRight now stores coords for what direction is to the right of the camera
         
-        //Player flying because camera look up no good
-        cameraForward.y = 0;
+        cameraForward.y = 0;//Player flying because camera look up no good
         cameraRight.y = 0;
 
-        //Normalize will make them constant so the speed of player isn't irregular
-        cameraForward.Normalize();
+        cameraForward.Normalize();//Normalize will make them constant so the speed of player isn't irregular
         cameraRight.Normalize();
 
-        //playerDirection equals to the coords of where camera is facing and to the right of camera multiplied by x & z.
-        Vector3 playerDirection = cameraForward * z + cameraRight * x;
+        Vector3 playerDirection = cameraForward * z + cameraRight * x;//playerDirection equals to the coords of where camera is facing and to the right of camera multiplied by x & z.
 
-        
-        //The player controller for the player is the value of playerDirection multiplied by the float of playerSpeed multiplied by the realtime of the program so it moves at normal rate without being tied to FPS.
-        playerControl.Move(playerDirection * playerSpeed * Time.deltaTime);
 
-        rotatePlayer(cameraForward);//executes function
+        playerControl.Move(playerDirection * playerSpeed * Time.deltaTime);//The player controller for the player is the value of playerDirection multiplied by the float of playerSpeed multiplied by the realtime of the program so it moves at normal rate without being tied to FPS.
 
-        void rotatePlayer(Vector3 cameraForward)// defines the function being executed
+        rotatePlayer(cameraForward);
+
+        void rotatePlayer(Vector3 cameraForward)
         {
-            transform.rotation = Quaternion.LookRotation(cameraForward);//sets player rotation to rotation of cameraForward
+            transform.rotation = Quaternion.LookRotation(cameraForward);//sets player rotation to rotation of cameraForward using Quaternion function
         }
+
+        fallSpeed += playerGravity * Time.deltaTime;
+        playerControl.Move(Vector3.up * fallSpeed * Time.deltaTime); //calculates the way the player falls(gravity)
+
+
 
     }
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
