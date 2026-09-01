324a55fd00382336aebf4b1e680b728911bb891a
Surrealsr <stevenronda77@gmail.com>
2026-08-31 22:33:28 -0400

    began scripting ranged enemy
    -Added ability for ranged enemy to have an LOS (Later will implement same LOS to Melee enemy)
    -IW on model/Prefab for Projectile (SUPER EARLY)
    (DISCLAIMER)
    No notes in the Ranged Enemy script will add at later date

---
+++ Assets/Scripts/RangedEnemy.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class RangedEnemy : MonoBehaviour
+{
+    public Transform player;
+    public Transform firepoint;
+    public GameObject projectilePrefab;
+    public float attackRange = 15;
+    public float fireCooldown = 1f;
+    public LayerMask sightLayers;
+    private float fireTimer;
+
+    private void Update()
+    {
+        float distancetoplayer = Vector3.Distance(player.position, transform.position);
+        
+        if (fireTimer > 0)
+        {
+            fireTimer -= Time.deltaTime;
+        }
+        if (distancetoplayer <= attackRange)
+        {
+            if (CanSeePlayer())
+            {
+                LookAtPlayer();
+
+                if(fireTimer <= 0)
+                {
+                    Shoot();
+                    fireTimer = fireCooldown;
+                }
+            }
+        }
+    }
+    bool CanSeePlayer()
+    {
+        Vector3 direction = player.position - firepoint.position;
+
+        float distance = direction.magnitude;
+
+        direction.Normalize();
+
+        if (Physics.Raycast(firepoint.position,direction, out RaycastHit hit, distance, sightLayers))
+        {
+            if (hit.transform.CompareTag("Player"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    void LookAtPlayer()
+    {
+        Vector3 direction = player.position - transform.position;
+        direction.y = 0;
+        transform.rotation = Quaternion.LookRotation(direction);
+    }
+    void Shoot()
+    {
+        Instantiate(projectilePrefab, firepoint.position, firepoint.rotation);
+    }
+}
